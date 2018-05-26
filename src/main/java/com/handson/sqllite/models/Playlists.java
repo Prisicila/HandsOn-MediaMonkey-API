d051abe51b59ee91b70a47f04286f87daa2750bd
Silas Silva <silas_sansil@hotmail.com>
2018-05-26 10:34:13 -0300

    ajuste entidade musica

--- src/main/java/com/handson/sqllite/models/Playlists.java
+++ src/main/java/com/handson/sqllite/models/Playlists.java
@@ -4,7 +4,11 @@ import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="playlists")
@@ -14,7 +18,27 @@ public class Playlists {
     @GeneratedValue
     private String id;
 
+    @JoinTable(name = "PlaylistMusicas",
+            joinColumns = @JoinColumn(name = "playlistId"),
+            inverseJoinColumns = @JoinColumn(name = "musicaId")
+    )
+    private List<Music> musics = new ArrayList<>();
+
     public String getId() {
         return id;
+    }
+
+    public Playlists setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public List<Music> getMusics() {
+        return musics;
+    }
+
+    public Playlists setMusics(List<Music> musics) {
+        this.musics = musics;
+        return this;
     }
 }
--- src/main/java/com/handson/sqllite/resource/PlaylistResource.java
+++ src/main/java/com/handson/sqllite/resource/PlaylistResource.java
@@ -1,6 +1,11 @@
 package com.handson.sqllite.resource;
 
+import com.handson.sqllite.models.Artist;
+import com.handson.sqllite.models.Playlists;
 import com.handson.sqllite.service.PlaylistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,9 @@ public class PlaylistResource {
         this.playlistService =  playlistService;
     }
 
-
+    @GetMapping
+    public ResponseEntity<Iterable<Playlists>> findAll() {
+        return new ResponseEntity<>(this.playlistService.findAll(), HttpStatus.OK);
+    }
 
 }
