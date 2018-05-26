c643fa8323fa0d35b261414bbcc8ba08453ce1fa
Silas Silva <silas_sansil@hotmail.com>
2018-05-26 10:28:34 -0300

    ajuste entidade musica

--- src/main/java/com/handson/sqllite/models/Music.java
+++ src/main/java/com/handson/sqllite/models/Music.java
@@ -1,53 +1,52 @@
 package com.handson.sqllite.models;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "Musicas")
 public class Music {
-	@Id
-	private String id;
-	
-	private String nome;
-	
-    @ManyToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="ArtistaId")
+    @Id
+    private String id;
+
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ArtistaId")
     private Artist artist;
 
-    public Music() {
-    	super();
-    }
-    
+
     public Music(String id, String nome, Artist artist) {
-    	this.id = id;
-    	this.text = text;
-    	this.artist = artist;
+        this.id = id;
+        this.nome = nome;
+        this.artist = artist;
     }
-    
-	public String getId() {
-		return id;
-	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public Artist getArtist() {
-		return artist;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
 }
