ea01231327528170c37efd736d06dedda1332455
Silas Silva <silas_sansil@hotmail.com>
2018-05-26 10:17:03 -0300

    artista estrutura

--- src/main/java/com/handson/sqllite/models/Artist.java
+++ src/main/java/com/handson/sqllite/models/Artist.java
@@ -2,12 +2,14 @@ package com.handson.sqllite.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * @author silas.silva
  * @since 26/05/18
  */
 @Entity
+@Table(name = "Artistas")
 public class Artist {
 
     @Id
